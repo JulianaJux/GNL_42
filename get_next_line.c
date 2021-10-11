8aabcadaafd7e1a71442dcb1b5ac0fd26b70ad6c
Juliana Jux <julianalencar73@gmail.com>
2021-10-11 18:30:54 -0300

    achando o break

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,18 @@
 /*   By: jde-alen <jde-alen@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 16:01:08 by jde-alen          #+#    #+#             */
-/*   Updated: 2021/10/01 16:01:22 by jde-alen         ###   ########.fr       */
+/*   Updated: 2021/10/11 18:25:17 by jde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+static int	find_break(char *buff)
+{
+	if (!buff)
+		return (0);
+	if (ft_srchr(buff, '\n'))
+		return (1);
+	else
+	return (0);	
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,27 @@
 /*   By: jde-alen <jde-alen@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 16:01:00 by jde-alen          #+#    #+#             */
-/*   Updated: 2021/10/01 16:01:23 by jde-alen         ###   ########.fr       */
+/*   Updated: 2021/10/11 18:28:30 by jde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	j;
+
+	if (!s)
+		return (NULL);
+	j = 0;
+	while (s[j])
+	{
+		if (s[j] == (char)c)
+			return ((char *)(s + j));
+		j++;
+	}
+	if (s[j] == (char)c)
+		return ((char *)(s + j));
+	return (NULL);
+}
 
