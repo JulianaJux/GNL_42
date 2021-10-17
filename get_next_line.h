31c32628659fbda986d887457c8383bae21b293b
Juliana Jux <julianalencar73@gmail.com>
2021-10-17 20:59:21 -0300

    mandatório feito

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jde-alen <jde-alen@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 16:01:11 by jde-alen          #+#    #+#             */
-/*   Updated: 2021/10/01 16:03:39 by jde-alen         ###   ########.fr       */
+/*   Updated: 2021/10/17 20:58:47 by jde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,14 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *s);
+char	*ft_strcat(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(const char *str, size_t start, size_t len);
+char	*ft_bzero(size_t size);
+
+#endif
