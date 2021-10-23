ef484b0c34c14f58f115a895af78980b20fe5401
Juliana Jux <julianalencar73@gmail.com>
2021-10-23 16:30:05 -0300

    arquivos criados_teste do read

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jde-alen <jde-alen@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/23 16:20:42 by jde-alen          #+#    #+#             */
+/*   Updated: 2021/10/23 16:29:20 by jde-alen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+
+
+
+
+#endif
