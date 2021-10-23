74086547749f97dd100a74c5b768c74203b03310
Juliana Jux <julianalencar73@gmail.com>
2021-10-23 20:51:01 -0300

    teste + funções

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,132 @@
 /*   By: jde-alen <jde-alen@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 16:19:27 by jde-alen          #+#    #+#             */
-/*   Updated: 2021/10/23 16:23:07 by jde-alen         ###   ########.fr       */
+/*   Updated: 2021/10/23 20:36:45 by jde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+/*retorna a linha dentro da string ou file completo de acordo com o buffer*/
+static char	find_line(char *str)
+{
+	int j;
+	char *line;
+
+	j = 0;
+
+	if (!(str))
+		return (NULL);
+	while (*str != '\0' || *str != '\n')
+		j++;
+
+	line = (char *)malloc(size_of(char) * (j + 2));
+	if (!(line))
+		return (NULL);
+	ft_strcpy(line, str, j + 2);
+	if (line [0] == '\0')
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+/*não coube no utils, coloquei aqui*/
+static char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	len;
+	size_t	j;
+
+	if (!s1)
+		return (NULL);
+	len = ft_strlen(s1);
+	str = (char *)malloc(len + 1);
+	if (!str)
+		return (NULL);
+	j = 0;
+	while (j < len)
+	{
+		str[j] = s1[j];
+		j++;
+	}
+	str[len] = '\0';
+	return (str);
+}
+
+/*le e junta as linhas que li em um espaço*/
+static char read_and_join(int fd, char *str, char *space)
+{
+	int	reading;
+	int j;
+	char *tmp;
+
+	reading = 1
+	j = 0
+	space = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!space)
+		return(NULL); 	
+	while (j == 0 && reading != 0)
+	{
+ 		reading = read(fd, space, BUFFER_SIZE);
+		if (reading == - 1)
+		{
+			free(space);
+			return (NULL);
+		}
+		space[reading] = '\0'
+		if (!str)
+			str = ft_strdup("");
+		tmp = str;
+		str = ft_strjoin(tmp, space);
+		free(tmp);
+		if(ft_strch(str, '\n'))
+			j = 1;
+	}
+	free(space);
+	return (str);
+}
+
+static char	*new_save(char *str)
+{
+	int		j;
+	char	*new_save;
+
+	j = 0;
+	while(str[j] && str[j] != '\n')
+		j++;
+	if(str[j] == '\0')
+	{
+		free(str);
+		return (NULL);
+	}
+	new_save = (char *)malloc(sizeof(char) * (ft_strlen(str) - j + 1));
+	if (!new_save)
+		return (NULL);
+	ft_strlcpy(new_save, str + j + 1, ft_strlen(str) - j + 1);
+	free(str);
+	return (new_save);
+}
+
 char	*get_next_line(int fd)
 {
-	
+	static char *save_all;
+	char		*line;
+	char		*file;
+
+	line = NULL;
+	if(fd < 0 || BUFFER_SIZE < = 0)
+		return (NULL);
+	file = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!file)
+	{
+		free(file);
+		return(NULL);
+	}
+	save_all = read_and_join(fd, save_all, file);
+	if (!save_all)
+		return(NULL);
+	line = find_line(save_all);
+	save_all = new_save(save_all);
+	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,94 @@
 /*   By: jde-alen <jde-alen@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 16:19:52 by jde-alen          #+#    #+#             */
-/*   Updated: 2021/10/23 16:23:03 by jde-alen         ###   ########.fr       */
+/*   Updated: 2021/10/23 20:20:46 by jde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	ft_strlen(const char *s)
+{
+	unsigned int	j;
+
+	j = 0;
+	while (*s != '\0')
+	{
+		j++;
+		s++;
+	}
+	return (j);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srcsize;
+	size_t	j;
+
+	if (!src || !dst)
+		return (0);
+	srcsize = ft_strlen(src);
+	j = 0;
+	if (dstsize != 0)
+	{
+		while ((src[j] != '\0') && (j < (dstsize - 1)))
+		{
+			dst[j] = src[j];
+			j++;
+		}
+		dst[j] = '\0';
+	}
+	return (srcsize);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t			j;
+	unsigned char	*d;
+	unsigned char	*s;
+
+	j = 0;
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	while (j < n)
+	{
+		d[j] = s[j];
+		j++;
+	}
+	return (dst);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	char	*ostr;
+
+	if (s1 == 0 || s2 == 0)
+		return (0);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	ostr = str + ft_strlen(s1);
+	ft_memcpy(str, s1, ft_strlen(s1));
+	ft_memcpy(ostr, s2, ft_strlen(s2));
+	str[ft_strlen(s1) + ft_strlen(s2)] = 0;
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	j;
+
+	if (!s)
+		return (NULL);
+	j = 0;
+	while (s[j])
+	{
+		if (s[j] == (char)c)
+			return ((char *)(s + j));
+		j++;
+	}
+	if (s[j] == (char)c)
+		return ((char *)(s + j));
+	return (NULL);
+}
