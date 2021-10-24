04e329614521869db3bd6c65a6b47b96aad66033
Juliana Jux <julianalencar73@gmail.com>
2021-10-24 15:10:39 -0300

    finalizado, eu acho

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jde-alen <jde-alen@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 16:19:27 by jde-alen          #+#    #+#             */
-/*   Updated: 2021/10/23 21:02:39 by jde-alen         ###   ########.fr       */
+/*   Updated: 2021/10/24 15:08:23 by jde-alen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ static char	*find_line(char *str)
 		return (0);
 	while (*str != '\0' || *str != '\n')
 		j++;
-
 	line = (char *)malloc(sizeof(char) * (j + 2));
 	if (!(line))
 		return (NULL);
@@ -36,52 +35,28 @@ static char	*find_line(char *str)
 	}
 	return (line);
 }
-/*não coube no utils, coloquei aqui*/
-static char	*ft_strdup(const char *s1)
-{
-	char	*str;
-	size_t	len;
-	size_t	j;
-
-	if (!s1)
-		return (NULL);
-	len = ft_strlen(s1);
-	str = (char *)malloc(len + 1);
-	if (!str)
-		return (NULL);
-	j = 0;
-	while (j < len)
-	{
-		str[j] = s1[j];
-		j++;
-	}
-	str[len] = '\0';
-	return (str);
-}
 
 /*le e junta as linhas que li em um espaço*/
 static char *read_and_join(int fd, char *str, char *space)
 {
 	int	reading;
-	int j;
+	int	j;
 	char *tmp;
 
 	reading = 1;
 	j = 0;
 	space = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!space)
-		return(NULL); 	
-	while (j == 0 && reading != 0)
+		return(NULL);
+		while (j == 0 && reading != 0)
 	{
- 		reading = read(fd, space, BUFFER_SIZE);
+		reading = read(fd, space, BUFFER_SIZE);
 		if (reading == - 1)
 		{
 			free(space);
 			return (NULL);
 		}
 		space[reading] = '\0';
-		if (!str)
-			str = ft_strdup("");
 		tmp = str;
 		str = ft_strjoin(tmp, space);
 		free(tmp);
@@ -94,21 +69,16 @@ static char *read_and_join(int fd, char *str, char *space)
 
 static char	*new_save(char *str)
 {
-	int		j;
+	int	j;
 	char	*new_save;
 
 	j = 0;
-	while(str[j] && str[j] != '\n')
+	while(str[j] != '\0' && str[j] != '\n')
 		j++;
-	if(str[j] == '\0')
-	{
-		free(str);
-		return (NULL);
-	}
 	new_save = (char *)malloc(sizeof(char) * (ft_strlen(str) - j + 1));
 	if (!new_save)
 		return (NULL);
-	ft_strlcpy(new_save, str + j + 1, ft_strlen(str) - j + 1);
+	ft_strlcpy(new_save, str + 1, ft_strlen(str) - j + 1);
 	free(str);
 	return (new_save);
 }
@@ -122,15 +92,9 @@ char	*get_next_line(int fd)
 	line = NULL;
 	if(fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	file = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!file)
-	{
-		free(file);
-		return(NULL);
-	}
 	save_all = read_and_join(fd, save_all, file);
 	if (!save_all)
-		return(NULL);
+		return (NULL);
 	line = find_line(save_all);
 	save_all = new_save(save_all);
 	return (line);
--- main_get_2.c
+++ main_get_2.c
@@ -1,12 +1,13 @@
 #include "get_next_line.h"
-#include <stdio.h>
-#include <fcntl.h> // O_RDONLY
-int main()
+
+int main(void)
 {
-int fd = open("teste.txt", O_RDONLY);
-printf("->%s", get_next_line(0));
-printf("->%s", get_next_line(fd));
-printf("->%s", get_next_line(fd));
-printf("->%s", get_next_line(fd));
-printf("->%s", get_next_line(fd));
+	int fd;
+
+	fd = open("teste.txt", O_RDONLY);
+	printf("->%s", get_next_line(0));
+	printf("->%s", get_next_line(fd));
+	printf("->%s", get_next_line(fd));
+	printf("->%s", get_next_line(fd));
+	printf("->%s", get_next_line(fd));
 }
